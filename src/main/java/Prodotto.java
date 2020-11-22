b36b491b1914b84d4efd1837527726875a65f5d7
kronoidz <alessandro.pecugi@outlook.it>
2020-11-22 12:38:08 +0100

    implementata Prodotto

--- src/main/java/Prodotto.java
+++ src/main/java/Prodotto.java
@@ -1,2 +1,20 @@
 public class Prodotto {
+    private String descrizione;
+    private double prezzo;
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
 }
