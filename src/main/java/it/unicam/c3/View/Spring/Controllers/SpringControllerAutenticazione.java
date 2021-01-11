99d55152dcea4c41bc5bbb973e7828275524b8be
kronoidz <alessandro.pecugi@outlook.it>
2021-01-11 12:59:59 +0100

    modulo di autenticazione

--- src/main/java/it/unicam/c3/View/Spring/Controllers/SpringControllerAutenticazione.java
+++ src/main/java/it/unicam/c3/View/Spring/Controllers/SpringControllerAutenticazione.java
@@ -1,10 +1,18 @@
 package it.unicam.c3.View.Spring.Controllers;
 
-import it.unicam.c3.View.Spring.FormBeans.AutenticaAmministratoreRequest;
+import it.unicam.c3.Controller.ControllerAutenticazione;
+import it.unicam.c3.View.Spring.FormModels.Credenziali;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class SpringControllerAutenticazione {
@@ -14,9 +22,49 @@ public class SpringControllerAutenticazione {
         return "auth";
     }
 
-    @PostMapping("/auth-admin")
-    public String AutenticaAmministratore(AutenticaAmministratoreRequest request)
+    @PostMapping( value = "/do-auth")
+    public ModelAndView DoAuth(HttpSession session,
+                               Credenziali credenziali,
+                               BindingResult result,
+                               Model model )
     {
-        throw new NotImplementedException();
+        if (result.hasErrors()) {
+            model.addAttribute("error", "Invalid request");
+            return new ModelAndView("/auth", HttpStatus.BAD_REQUEST);
+        }
+        else {
+            ControllerAutenticazione auth;
+            if (session.isNew()) {
+                auth = new ControllerAutenticazione();
+                session.setAttribute("auth", auth);
+            }
+            else auth = (ControllerAutenticazione) session.getAttribute("auth");
+
+            String email = credenziali.getEmail();
+            String pwd = credenziali.getPassword();
+
+            Object controller = null;
+
+            switch (credenziali.getTipoUtente()) {
+                case "cliente":
+                  controller = auth.autenticaCliente(email, pwd);
+                  break;
+                case "commerciante":
+                    controller = auth.autenticaCommerciante(email, pwd);
+                    break;
+                case "corriere":
+                    controller = auth.autenticaCorriere(email, pwd);
+                    break;
+            }
+
+            session.setAttribute("controller", controller);
+
+            if (controller == null) {
+                model.addAttribute("error", "Credenziali invalide");
+                return new ModelAndView("/auth", HttpStatus.UNAUTHORIZED);
+            }
+
+            return new ModelAndView("/home-" + credenziali.getTipoUtente());
+        }
     }
 }
--- src/main/java/it/unicam/c3/View/Spring/SpringView.java
+++ src/main/java/it/unicam/c3/View/Spring/SpringView.java
@@ -1,5 +1,8 @@
 package it.unicam.c3.View.Spring;
 
+import it.unicam.c3.Anagrafica.Commerciante;
+import it.unicam.c3.Anagrafica.Corriere;
+import it.unicam.c3.Citta.CentroCittadino;
 import it.unicam.c3.View.View;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +11,30 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class SpringView implements View {
+    @SuppressWarnings("RedundantThrows")
     @Override
     public void start() throws IOException {
+
+        MakeTestData(); // todo: DEBUG ONLY
+
         SpringApplication.run(SpringView.class);
+    }
+
+    private void MakeTestData() {
+        CentroCittadino.getInstance().addCommerciante (
+                new Commerciante (
+                        "Alessandro",
+                        "Pecugi",
+                        "alessandro.pecugi@example.com",
+                        "ciaone" )
+        );
+
+        CentroCittadino.getInstance().addCorriere (
+                new Corriere(
+                        "Corriere",
+                        "Corrieri",
+                        "c.corrieri@example.com",
+                        "ciaone" )
+        );
     }
 }
